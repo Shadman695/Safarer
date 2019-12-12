13b45f0c89e6747b041fd12281028021d58f96cb
ahshanhabib0175 <58226519+ahshanhabib0175@users.noreply.github.com>
2019-12-12 14:20:52 +0600

    layout bar/post
    layout bar/post

--- app/src/main/java/com/example/safarer/MainActivity.java
+++ app/src/main/java/com/example/safarer/MainActivity.java
@@ -1,46 +1,57 @@
 package com.example.safarer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< Updated upstream
+//<<<<<<< Updated upstream
 import android.widget.ImageButton;
-=======
+//=======
 import android.widget.Button;
->>>>>>> Stashed changes
+//>>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
 
-
     private ImageButton AddNewPostButton;
+
+    private Toolbar mToolbar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+
+        AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);
 
 
-        //AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);
         AddNewPostButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                sendUserToPostActivity();
+            public void onClick(View v)
+            {
+                SendUserToPostActivity();
             }
-
         });
 
-        //DisplayAllUsersPosts();
+
+
+
+
     }
 
+    private void SendUserToPostActivity()
 
+    {
 
-    private void sendUserToPostActivity() {
-
-            Intent addNewPostIntent = new Intent(MainActivity.this, PostActivity.class);
-            startActivity(addNewPostIntent);
-
+        Intent addNewPostIntent = new Intent(MainActivity.this, PostActivity.class);
+        startActivity(addNewPostIntent);
     }
 }
