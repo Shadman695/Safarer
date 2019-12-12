51b38bc343ef8d17f00833b422330e68fc01fe48
ahshanhabib0175 <58226519+ahshanhabib0175@users.noreply.github.com>
2019-12-12 11:33:40 +0600

    PostActivity
    PostActivity

--- app/src/main/java/com/example/safarer/MainActivity.java
+++ app/src/main/java/com/example/safarer/MainActivity.java
@@ -2,14 +2,37 @@ package com.example.safarer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ImageButton AddNewPostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);
+
+
+    }
+    AddNewPostButton.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+                public void onClick(View v)
+        {
+            sendUserToPostActivity();
+        }
+    }
+
+    private void sendUserToPostActivity() {
+
+            Intent addNewPostIntent = new Intent(MainActivity.this, PostActivity.class);
+            startActivity(addNewPostIntent);
 
     }
 }
