fc32ffcea527f8d6e9e93c0f8cb3b675e2e4187e
Shadman <shadman.khan@northsouth.edu>
2019-12-12 13:41:31 +0600

    Registration page XML

--- app/src/main/java/com/example/safarer/MainActivity.java
+++ app/src/main/java/com/example/safarer/MainActivity.java
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< Updated upstream
+
 import android.widget.ImageButton;
-=======
+
 import android.widget.Button;
->>>>>>> Stashed changes
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,27 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-
-
-        //AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);
-        AddNewPostButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendUserToPostActivity();
-            }
-
-        });
-
-        //DisplayAllUsersPosts();
-    }
-
-
-
-    private void sendUserToPostActivity() {
-
-            Intent addNewPostIntent = new Intent(MainActivity.this, PostActivity.class);
-            startActivity(addNewPostIntent);
 
     }
 }
